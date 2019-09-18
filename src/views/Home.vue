41733d567e39ebe162efe6bca3f4bf62545f79ff
Ibrahim Joseph <josephibrahi@gmail.com>
2019-09-18 23:03:20 +0100

    add  home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,150 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <!-- <StarwarsHead ms="hello" /> -->
+    <!-- <StarwarTitle title="Popular Starship" /> -->
+    <div class="card-flex">
+      <!-- <StarShip />
+      <StarShip />
+      <StarShip />
+      <StarShip />
+      <StarShip />
+      <StarShip />-->
+      <!-- <div class="col-md-6 mx-auto my-5 d-flex justify-content-center align-items-center">
+        <button class="btn-lg col-md-8">VIEW MORE</button>
+      </div>-->
+
+      <!-- <div class="fluid px-5 py-4">
+        <div class="row">
+          <div class="card" style="width: 36rem;">
+            <div class="row m-0">
+              <div class="col-sm-6 p-0">
+                <img src="../assets/character-1.jpg" class="w-100 h-100 h-sm-100" />
+              </div>
+              <div class="col-sm-6 p-0 py-4">
+                <div class="card-block px-3">
+                  <h4 class="card-title">Lorem ipsum dolor sit amet</h4>
+                  <p
+                    class="card-text"
+                  >Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="fluid px-5 py-4">
+        <div class="row">
+          <div class="card" style="width: 36rem;">
+            <div class="row m-0">
+              <div class="col-sm-6 p-0">
+                <img src="../assets/character-1.jpg" class="w-100 h-100 h-sm-100" />
+              </div>
+              <div class="col-sm-6 p-0 py-4">
+                <div class="card-block px-3">
+                  <h4 class="card-title">Lorem ipsum dolor sit amet</h4>
+                  <p
+                    class="card-text"
+                  >Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="fluid px-5 py-4">
+        <div class="row">
+          <div class="card" style="width: 36rem;">
+            <div class="row m-0">
+              <div class="col-sm-6 p-0">
+                <img src="../assets/character-1.jpg" class="w-100 h-100 h-sm-100" />
+              </div>
+              <div class="col-sm-6 p-0 py-4">
+                <div class="card-block px-3">
+                  <h4 class="card-title">Lorem ipsum dolor sit amet</h4>
+                  <p
+                    class="card-text"
+                  >Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="fluid px-5 py-4">
+        <div class="row">
+          <div class="card" style="width: 36rem;">
+            <div class="row m-0">
+              <div class="col-sm-6 p-0">
+                <img src="../assets/character-1.jpg" class="w-100 h-100 h-sm-100" />
+              </div>
+              <div class="col-sm-6 p-0 py-4">
+                <div class="card-block px-3">
+                  <h4 class="card-title">Lorem ipsum dolor sit amet</h4>
+                  <p class="card-text">
+                    Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    <a
+                      href="#"
+                    >Readmore</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>-->
+    </div>
+    <!-- <StarwarTitle title="Popular Planets" />
+    <PlanetSlide />
+    <StarwarTitle title="Popular Characters" />-->
+
+    <!-- <StarwarsCharacterCard />
+    <StarwarsCharacterCard />
+    <StarwarsCharacterCard />-->
+
+    <div class="container">
+      <div class="row">
+        <nav aria-label="...">
+          <ul class="pagination pagination-lg">
+            <li class="page-item disabled">
+              <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span class="sr-only">Previous</span>
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span class="sr-only">Next</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </main>
 </template>
 
+//
 <script>
+// import StarwarsHead from '@/components/reusable/StarwarsHead.vue';
+// import StarwarTitle from '@/components/reusable/StarwarTitle.vue';
+// import StarShip from '@/components/reusable/StarShip.vue';
+// import PlanetSlide from '@/components/reusable/PlanetSlide.vue';
+// import StarwarsCharacterCard from '@/components/reusable/StarwarsCharacterCard.vue';
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import LandingPage from '@/components/LandingPage.vue';
 
 export default {
   name: 'home',
+  props: {},
   components: {
-    HelloWorld,
+    // StarwarsHead,
+    // StarwarTitle,
+    // StarShip,
+    // PlanetSlide,
+    // StarwarsCharacterCard,
   },
 };
 </script>
+
+<style lang="css" scoped></style>
