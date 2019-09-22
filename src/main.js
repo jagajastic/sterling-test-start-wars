bf3d34ceaa7bdac6eb78219157bc57c62e784dca
Ibrahim Joseph <josephibrahi@gmail.com>
2019-09-22 15:02:36 +0100

    add this tweak for cors to allow me consume the api

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common['Accept'] = 'application/json';
+
 Vue.config.productionTip = false;
 
 new Vue({
