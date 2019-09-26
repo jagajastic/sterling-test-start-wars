1d7b5a2a4991fdc05d28b2e81f033c0d531bf61b
Ibrahim Joseph <josephibrahi@gmail.com>
2019-09-26 14:38:20 +0100

    add box shadow on starship

--- src/components/reusable/StarShip.vue
+++ src/components/reusable/StarShip.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="d-flex flex-row p-md-4 mx-auto py-2">
-    <div class="card mx-md-2 my-md-2" style="width: 18rem;">
+    <div class="card mx-md-2 my-md-2 rounded-0" style="width: 18rem;">
       <img src="../../assets/starship-1.jpg" class="card-img-top" alt="..." />
-      <div class="card-body">
+      <div class="card-body" id="box-shadow">
         <h5 class="card-title">{{ planet.name }}</h5>
         <p class="card-text">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
           suscipit quis quod corporis eligendi voluptatibus, nostrum adipisci
           fugiat unde
-          <a href="#" class="btn btn-secondary float-right rounded-0 text-dark">
-            Read More
-            <img src="../../assets/left-arrow.svg" id="arrow" />
-          </a>
+          <router-link to="/shipdetails">
+            <a class="btn btn-secondary float-right rounded-0 text-dark">
+              Read More
+              <img src="../../assets/left-arrow.svg" id="arrow" />
+            </a>
+          </router-link>
         </p>
       </div>
     </div>
@@ -27,6 +29,11 @@ export default {
 };
 </script>
 <style lang="css" scoped>
+#box-shadow {
+  border: 1px solid;
+  padding: 10px;
+  box-shadow: -5px 10px #afafaf;
+}
 #arrow {
   transform: scaleX(-1);
 }
