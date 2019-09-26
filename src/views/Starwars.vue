6b0b429e315f93e6bddfa55c73fd07aabde9ad96
Ibrahim Joseph <josephibrahi@gmail.com>
2019-09-26 04:10:09 +0100

    fetching data from the starwar api

--- src/views/Starwars.vue
+++ src/views/Starwars.vue
@@ -1,16 +1,46 @@
 <template>
-  <div>
-    //- <StarwarsHead />
-    <router-view />
-  </div>
+  <main>
+    <StarwarsHead />
+    <StarwarTitle title="Popular Starships" />
+    <div class="card-flex">
+      <StarShip
+        v-for="planet in planets"
+        v-bind:key="planet.name"
+        :planet="planet"
+      />
+    </div>
+    <Navigation />
+  </main>
 </template>
 
 <script>
-// import StarwarsHead from '@/components/reusable/StarwarsHead.vue';
+import StarwarsHead from '@/components/reusable/StarwarsHead.vue';
+import StarwarTitle from '@/components/reusable/StarwarTitle';
+import StarShip from '@/components/reusable/StarShip';
+import Navigation from '../components/reusable/Navigation';
+
+import axios from 'axios';
+
 export default {
   name: 'starwars',
+  props: {
+    planets: [],
+  },
   components: {
-    // StarwarsHead,
+    StarwarsHead,
+    StarwarTitle,
+    StarShip,
+    Navigation,
+  },
+  created: function() {
+    axios('https://swapi.co/api/starships')
+      .then(response => {
+        this.planets = response.data.results;
+        console.log(response);
+      })
+      .catch(error => {
+        console.error;
+      });
   },
 };
 </script>
