22af5350d6a8a921ac241c9714100a861a13f13d
Ibrahim Joseph <josephibrahi@gmail.com>
2019-09-26 14:38:49 +0100

    add link to readmore

--- src/components/reusable/StarwarsCharacterCard.vue
+++ src/components/reusable/StarwarsCharacterCard.vue
@@ -17,7 +17,7 @@
                 labore et dolore magna aliqua. Ut enim ad minim veniam, quis
                 nostrud exercitation ullamco laboris nisi ut aliquip ex ea
                 commodo consequat.
-                <a href="#">Readmore</a>
+                <router-link to="/characterDetails">Readmore</router-link>
               </p>
             </div>
           </div>
