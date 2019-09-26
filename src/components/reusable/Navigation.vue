a11f54d5f5db823e596ab2b24d80c88b6e18d1cf
Ibrahim Joseph <josephibrahi@gmail.com>
2019-09-26 04:06:53 +0100

    add functionality to my page navigation

--- src/components/reusable/Navigation.vue
+++ src/components/reusable/Navigation.vue
@@ -3,15 +3,26 @@
     <div class="row">
       <nav aria-label="...">
         <ul class="pagination pagination-lg">
-          <div class="my-3 mx-3">1 of 10</div>
-          <li class="page-item disabled">
-            <a class="page-link" href="#" aria-label="Previous">
+          <div class="my-3 mx-3">
+            {{ page - pageSize }} - {{ Math.ceil(count / 10) }}
+          </div>
+          <li class="page-item">
+            <a
+              class="page-link"
+              href="#"
+              aria-label="Previous"
+              @click.prevent="previousPage"
+            >
               <span aria-hidden="true">&laquo;</span>
               <span class="sr-only">Previous</span>
             </a>
           </li>
           <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
+            <a
+              class="page-link link"
+              aria-label="Next"
+              @click.prevent="nextPage"
+            >
               <span aria-hidden="true">&raquo;</span>
               <span class="sr-only">Next</span>
             </a>
@@ -25,6 +36,19 @@
 <script>
 export default {
   name: 'Navigation',
-  props: {},
+  props: {
+    count: Number,
+    next: String,
+    page: String,
+    pageSize: String,
+  },
+  methods: {
+    nextPage() {
+      this.$emit('nextPage');
+    },
+    previousPage() {
+      this.$emit('previousPage');
+    },
+  },
 };
 </script>
