aa9f631184f6c3a07096778d81095986863b31eb
jagajastic <josephibrahi@gmail.com>
2019-09-27 16:51:16 +0100

    add margin and padding to starwar ship

--- src/components/reusable/StarShip.vue
+++ src/components/reusable/StarShip.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="d-flex flex-row p-md-4 mx-auto py-2">
-    <div class="card mx-md-2 my-md-2 rounded-0" style="width: 18rem;">
+    <div class="card mx-md-2 my-md-2 rounded-0 border-0" id="card-w">
       <img src="../../assets/starship-1.jpg" class="card-img-top" alt="..." />
-      <div class="card-body" id="box-shadow">
+      <div class="card-body p-4" id="box-shadow">
         <h5 class="card-title">{{ planet.name }}</h5>
         <p class="card-text">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
           suscipit quis quod corporis eligendi voluptatibus, nostrum adipisci
           fugiat unde
-          <router-link :to="'/shipdetails/' + planet" :planet="planet">
-            <a class="btn btn-secondary float-right rounded-0 text-dark">
-              Read More
-              <img src="../../assets/left-arrow.svg" id="arrow" />
-            </a>
-          </router-link>
         </p>
+        <router-link
+          :to="{ path: '/shipdetails/' + planet.name }"
+          class="btn float-right rounded-0 text-dark"
+          style="background-color: #d8d8d8; margin:10px;"
+        >
+          Read More
+          <img src="../../assets/left-arrow.svg" id="arrow" />
+        </router-link>
       </div>
     </div>
   </div>
@@ -29,11 +31,24 @@ export default {
 };
 </script>
 <style lang="css" scoped>
-#box-shadow {
-  border: 1px solid;
-  padding: 10px;
-  box-shadow: -5px 10px #afafaf;
+#card-w {
+  width: 23em;
+  background-color: #d8d8d8;
 }
+#inner-box {
+  padding: 5px;
+  background-color: #d8d8d8;
+}
+
+#box-shadow {
+  padding: 0px;
+  padding-left: 6px;
+  background-color: #f2f2f2;
+  margin-left: 8px;
+  margin-bottom: 8px;
+  margin-right: -8px;
+}
+
 #arrow {
   transform: scaleX(-1);
 }
