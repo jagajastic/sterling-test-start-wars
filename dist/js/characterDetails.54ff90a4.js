50912ddbd986a3f0c03b1c465123d4b5e8544ec0
jagajastic <josephibrahi@gmail.com>
2019-09-27 09:25:33 +0100

    initial deployment

---
+++ dist/js/characterDetails.54ff90a4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["characterDetails"],{ee5e:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("h1",[e._v("Details")])])}],c={name:"CharacterDetails"},l=c,s=n("2877"),i=Object(s["a"])(l,a,r,!1,null,null,null);t["default"]=i.exports}}]);
+//# sourceMappingURL=characterDetails.54ff90a4.js.map
